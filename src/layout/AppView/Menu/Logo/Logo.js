2680119cb2df81d3ef6dcc30ddd0ac2481c383db
reallxl <reallxl@hotmail.com>
2018-08-05 13:12:53 +0800

    update logo

--- src/layout/AppView/Menu/Logo/Logo.js
+++ src/layout/AppView/Menu/Logo/Logo.js
@@ -5,10 +5,10 @@ import classes from './Logo.css';
 const Logo = (props) => {
   return (
     <div className={ classes.Logo }>
-      <span className={ classes.McPart }>M</span>
-      <span className={ classes.McPart } style={ { fontSize: '16px', } }>c</span>
-      <span className={ classes.APart }>A</span>
-      <span className={ classes.ListPart }>List</span>
+      <div className={ classes.McPart }>M</div>
+      <div className={ classes.McPart } style={ { fontSize: '16px', } }>c</div>
+      <div className={ classes.APart }>A</div>
+      <div className={ classes.ListPart }>List</div>
     </div>
   );
 };
